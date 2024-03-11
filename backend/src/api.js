bb8c5071a3c150ac62b581796d5be2f926e174c7
Karlos Messyas <karlosmessyas14@gmail.com>
2024-03-11 14:53:39 -0300

    Adicionando rotas de adicionar e remover Tasks

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const router = require('./router')
-
 const api = express();
+api.use(express.json());
+
+const router = require('./router');
 api.use(router);
 
 module.exports = api;
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const tasksController = require('./controllers/taskController')
+const tasksController = require('./controllers/taskController');
+const tasksMiddleware = require('./middlewares/tasksMiddlewares');
 
 const router = express.Router();
 
 router.get('/tasks', tasksController.getAll);
+router.post('/tasks', tasksMiddleware.validatBody, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
 
 module.exports = router;
