4f029042f27681794e4249e14949003f3c3fb52e
Karlos Messyas <karlosmessyas14@gmail.com>
2024-03-13 16:00:34 -0300

    Adicionando Cors

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = requiere('cors');
 const api = express();
 api.use(express.json());
 
 const router = require('./router');
+api.use(cors());
 api.use(router);
 
 module.exports = api;
